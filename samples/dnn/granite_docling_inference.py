fe1672564299ca4e2f97eef3d1b96a709faa0f00
Jaivardhan Bhola <jaivardhan.bhola@bigvisionllc.com>
2026-07-17 21:20:55 +0530

    code cleanup

--- samples/dnn/granite_docling_inference.py
+++ samples/dnn/granite_docling_inference.py
@@ -1,8 +1,6 @@
 # This file is part of OpenCV project.
 # It is subject to the license terms in the LICENSE file found in the top-level directory
 # of this distribution and at http://opencv.org/license.html.
-# Copyright (C) 2026, BigVision LLC, all rights reserved.
-# Third party copyrights are property of their respective owners.
 
 '''
 This is a sample script to run Granite-Docling-258M vision-language inference in OpenCV
